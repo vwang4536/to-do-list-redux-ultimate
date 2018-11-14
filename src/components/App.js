f2318698d9132a3a24c22ad677675b4e5bd2bf46
Edward Roh <Admin@Eds-MacBook-Pro.local>
2018-11-14 15:24:08 -0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+import List from './List';
+
+const App = () => (
+  <div>
+    <h1>Hello My Classy Friend</h1>
+    <Form />
+    <List />
+  </div>
+);
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List = () => {
+
+  return (
+    <div>
+      <ul>
+        list
+      </ul>
+    </div>
+  )
+}
+
+export default List;
